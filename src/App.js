328d9f7e1b287adb3464d2b103e62a52d38a3296
Usama-Aslam <uaslam345@gmail.com>
2018-08-20 17:55:04 +0500

    i update my function and use arrow function

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ class App extends Component {
   constructor(){
     super();
     this.state={
-      count:0
+      count:0,
+      count2:0
     }
+      // this.setSomething = this.setSomething.bind(this)
   }
 
   renderHeader(){
@@ -24,17 +26,27 @@ class App extends Component {
                "this.setState(&#123;&#125;)"<br></br>
                and the use of bind in the for functions so that they can refer "this". <br></br><br></br>
                 
-                <button onClick={this.setSomeValue.bind(this)}>Click me</button><span>  onclick function "this.setSomeValue.bind(this)"</span> {/* using bind in direct function calling onClick*/}
+               {/*  using bind in direct function calling onClick */}
+                <button onClick={this.setSomeValue.bind(this)}>Click me</button><span>  onclick function "this.setSomeValue.bind(this)"</span>
                 <p>{this.state.count}</p>
 
-                <button onClick={()=>this.setSomeValue()}>Click me</button><span> binding by using arrow function </span>
+               {/* using bind by using arrow function calling. */}
+                <button onClick={()=>this.setAnotherSomeValue()}>Click me</button><span> binding by using arrow function "()=>&#123;this.setAnotherValue() &#125;" </span>
+                <p>{this.state.count2}</p>
              </p>
   }
   setSomeValue(){
     this.setState({
-      count:++this.state.count
+      count:++this.state.count,
     })
     console.log(this.state.count)
+  }
+
+  setAnotherSomeValue(){
+    this.setState({
+      count2:++this.state.count2
+    })
+    console.log(this.state.count2)
   }
 
   render() {
